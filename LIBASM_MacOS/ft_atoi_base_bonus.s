072e18168f7c3f4f4d97f9906047b1383d27ce93
allaeddine khalid <allaeddinekhalid@gmail.com>
2021-02-10 16:32:44 +0100

    ft_atoi_base_bonus

--- LIBASM_MacOS/ft_atoi_base_bonus.s
+++ LIBASM_MacOS/ft_atoi_base_bonus.s
@@ -1,5 +1,58 @@
 section .text
     global _ft_atoi_base
+    extern _ft_strlen
 
 _ft_atoi_base:
-    
+    push rbx
+    push rdi
+    mov rdi, rsi
+    call _ft_strlen
+    pop rdi
+    cmp rax, 1
+    jle error
+    mov r9, rax
+    xor rcx, rcx
+    xor rdx, rdx
+    xor r11, r11
+    atoi_base:
+        cmp BYTE [rdi + r11], 0x0
+        je end
+        check_str:
+            mov dl, BYTE [rdi + r11]
+            mov dh, BYTE [rsi + rcx]
+            cmp dl, dh
+            je atoi
+            cmp BYTE [rsi + rcx], 0x0
+            je atoi
+            inc rcx
+            jmp check_str
+        atoi:
+            cmp BYTE [rsi + rcx], 0x0
+            je error
+            call _ft_strlen
+            mov r8, rax
+            sub r8, r11
+            sub r8, 1
+            mov rax, 1
+            xor r10, r10
+            power:
+                cmp r10, r8
+                jge back
+                mul r9
+                inc r10
+                jmp power
+        back:
+            mul rcx
+            add rbx, rax
+            inc r11
+            jmp atoi_base
+    end:
+        cmp r11, 0
+        je error
+        mov rax, rbx
+        pop rbx
+        ret
+    error:
+        mov rax, 0
+        pop rbx
+        ret
--- LIBASM_MacOS/main.c
+++ LIBASM_MacOS/main.c
@@ -19,12 +19,12 @@ extern int		ft_strcmp(const char *s1, const char *s2);
 extern ssize_t	ft_write(int fd, const void *buf, size_t count);
 extern ssize_t	ft_read(int fd, void *buf, size_t count);
 extern char		*ft_strdup(const char *s);
-extern 
+extern int		ft_atoi_base(char *str, char *base);
 
 int main()
 {
 	// if ((ft_read(-1, line, 9) == -1))
 	// 	printf("\033[1;31mERROR: %s\033[0m\n", strerror(errno));
-	
+	printf("%d", ft_atoi_base("111", "01"));
 	return (0);
 }
