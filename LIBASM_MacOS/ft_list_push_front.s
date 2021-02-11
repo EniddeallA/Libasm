f51c2832628de2769aa134c02a7bf0481492d1f2
allaeddine khalid <allaeddinekhalid@gmail.com>
2021-02-11 17:01:06 +0100

    ft_list_push_front_bonus.s

--- LIBASM_MacOS/ft_list_push_front.s
+++ LIBASM_MacOS/ft_list_push_front.s
@@ -1,5 +1,21 @@
 section .text
     global _ft_list_push_front
+    extern _malloc
 
 _ft_list_push_front:
-    
+    push rsp
+    push rdi
+    push rsi
+    mov rdi, 16
+    call _malloc
+    pop rsi
+    pop rdi
+    cmp rax, 0x0
+    jz return
+    mov [rax], rsi
+    mov rcx, [rdi]
+    mov [rax + 8], rcx
+    mov [rdi], rax
+return:
+    pop rsp
+    ret
