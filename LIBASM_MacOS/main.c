91a6306a8a04b1bcd2da35536f2dd5e783843c23
allaeddine khalid <allaeddinekhalid@gmail.com>
2021-02-15 16:58:15 +0100

    adding .gitgnore + cleaning repo

--- LIBASM_MacOS/main.c
+++ LIBASM_MacOS/main.c
@@ -7,24 +7,12 @@
 #include "stdlib.h"
 #include "errno.h"
 
-typedef struct		s_list
-{
-	void			*data;
-	struct s_list	*next;
-}					t_list;
-
 extern size_t	ft_strlen(char *str);
 extern char		*ft_strcpy(char *dest, const char *src);
 extern int		ft_strcmp(const char *s1, const char *s2);
 extern ssize_t	ft_write(int fd, const void *buf, size_t count);
 extern ssize_t	ft_read(int fd, void *buf, size_t count);
 extern char		*ft_strdup(const char *s);
-extern int		ft_atoi_base(char *str, char *base);
-extern void		ft_list_push_front(t_list **begin_list, void *data);
-extern int		ft_list_size(t_list *begin_list);
-extern void		ft_list_sort(t_list **begin_list, int (*cmp)());
-extern void		ft_list_remove_if(t_list **begin_list, void *data_ref, 
-						int (*cmp)(), void (*free_fct)(void *));
 
 
 // void		ft_list_push_front(t_list **begin_list, void *data)
